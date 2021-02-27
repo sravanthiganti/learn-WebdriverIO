8ea8a60120854a6d494117fdfb753b4e55f1aea7
SravKarra <marutiskarra89@gmail.com>
2021-02-26 19:26:45 -0500

    Update google-search.e2e.js

--- test/specs/google-search.e2e.js
+++ test/specs/google-search.e2e.js
@@ -1,5 +1,31 @@
+const { isExportDeclaration } = require("typescript")
+
 describe('ebay website search', () =>{
-   it("verify title",()=>{
-       browser.url('https://www.costco.ca')
-   })
-})
+   it("should open the main URL and verify the title",()=>{
+       browser.url('https://www.ebay.ca/')
+       expect(browser).toHaveTitle(
+           'Electronics, Cars, Fashion, Collectibles & More | eBay'
+    );
+   });
+
+
+it('should search for product and verify the text value',()=>{
+    const searchInput = $('#gh-ac');
+    const searchBtn = $('#gh-btn');
+
+    searchInput.addValue('LapTop');
+    searchBtn.click();
+
+    expect(searchInput).toHaveValue('LapTop');
+});
+
+it('should redirect to a new page and verify the title',()=>{
+    expect(browser).toHaveTitle('LapTop| eBay');
+});
+
+it('should update the serach category',()=>{
+    const category=$('#gh-cat option:nth-child(1)');
+
+    expect(category).toHaveText('PC Laptops & Netbooks')
+});
+});
